ffc3b586a20809507ddd0056abb368e4c035350d
cgmcconnell214 <cgmcconnell214@gmail.com>
2025-08-10 17:08:36 -0500

    chore: include order modal component?

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
     environment: 'jsdom',
+    globals: true,
     coverage: {
       reporter: ['text', 'html', 'json'],
       provider: 'v8'
